3a0352d9effec601eac9245ea1c9a008d0f89ad1
Dzianis Vasileuski <d.vasileuski@gmail.com>
2017-10-21 20:58:14 +0300

    minor changes

--- src/main/java/by/vasilevsky/ice_tooll/Application.java
+++ src/main/java/by/vasilevsky/ice_tooll/Application.java
@@ -1,11 +1,9 @@
 package by.vasilevsky.ice_tooll;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan("by.vasilevsky.ice_tooll")
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) throws Exception {
